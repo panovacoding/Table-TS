f611f5abae8d657920446aa39efc0295fdd27ee2
Konstantin Rev <kykp@yandex.ru>
2024-04-07 15:13:22 +0400

    dev: changed style to copmany table

--- src/shared/ui/Table/ui/Table/Table.tsx
+++ src/shared/ui/Table/ui/Table/Table.tsx
@@ -1,5 +1,6 @@
 import {TableHeader} from "../TableHeader/TableHeader.tsx";
 import {TableBody} from "../TableBody/TableBody.tsx";
+import clsx from "clsx";
 
 import cls from './Table.module.scss';
 import {TableConfig} from "../../types/type.ts";
@@ -9,15 +10,16 @@ interface TableData {
 }
 
 interface TableProps<T> {
-  config: TableConfig<T>,
-  data: T[]
+  config: TableConfig<T>;
+  data: T[];
+  classNames?: string;
 }
 
 export const Table =  <T extends TableData>(props: TableProps<T>) => {
-  const {config, data} = props;
+  const {config, data, classNames} = props;
 
   return (
-    <table className={cls.Table}>
+    <table className={clsx(cls.Table, classNames)}>
       <TableHeader config={config}/>
       <TableBody data={data} config={config} />
     </table>
--- src/widgets/CompanyTable/ui/CompanyTable/CompanyTable.tsx
+++ src/widgets/CompanyTable/ui/CompanyTable/CompanyTable.tsx
@@ -1,10 +1,14 @@
 import {Table} from "@/shared/ui/Table";
 import {TableData} from '../../config/tableData.ts';
 import {TableConfigData} from "@/widgets/CompanyTable/config/TableConfig.ts";
-
+import cls from './CompanyTable.module.scss';
 
 export const CompanyTable = () => {
   return (
-    <Table config={TableConfigData} data={TableData}/>
+    <Table
+      config={TableConfigData}
+      data={TableData}
+      classNames={cls.Table}
+    />
   )
 }
